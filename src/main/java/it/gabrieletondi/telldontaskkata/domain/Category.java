820886f34e71a3ef6377c3feff4c0b41655e1bb8
xing <xing@integral.io>
2020-01-20 23:02:36 -0500

    Inline internal usage of getter with field.

--- src/main/java/it/gabrieletondi/telldontaskkata/domain/Category.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/domain/Category.java
@@ -12,11 +12,7 @@ public class Category {
         this.taxPercentage = taxPercentage;
     }
 
-    public BigDecimal getTaxPercentage() {
-        return taxPercentage;
-    }
-
     public BigDecimal calculateUnitaryTax(BigDecimal price) {
-        return price.divide(valueOf(100)).multiply(getTaxPercentage()).setScale(2, HALF_UP);
+        return price.divide(valueOf(100)).multiply(taxPercentage).setScale(2, HALF_UP);
     }
 }
