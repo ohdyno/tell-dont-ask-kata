efe39f0826dbdd49677ef2a281c11bfc475e131b
xing <xing@integral.io>
2020-01-20 21:23:53 -0500

    Encapsulate process sell item request into the request itself.

--- src/main/java/it/gabrieletondi/telldontaskkata/useCase/SellItemRequest.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/useCase/SellItemRequest.java
@@ -1,5 +1,12 @@
 package it.gabrieletondi.telldontaskkata.useCase;
 
+import it.gabrieletondi.telldontaskkata.domain.Order;
+import it.gabrieletondi.telldontaskkata.domain.OrderItem;
+import it.gabrieletondi.telldontaskkata.domain.Product;
+import it.gabrieletondi.telldontaskkata.repository.ProductCatalog;
+
+import java.math.BigDecimal;
+
 public class SellItemRequest {
     private int quantity;
     private String productName;
@@ -18,5 +25,27 @@ public class SellItemRequest {
 
     public String getProductName() {
         return productName;
+    }
+
+    void process(Order order, ProductCatalog productCatalog) {
+        Product product = productCatalog.getByName(getProductName());
+
+        if (product == null) {
+            throw new UnknownProductException();
+        }
+        else {
+            final BigDecimal taxedAmount = product.calculateTaxedAmount(this);
+            final BigDecimal taxAmount = product.calculateTaxAmount(this);
+
+            final OrderItem orderItem = new OrderItem();
+            orderItem.setProduct(product);
+            orderItem.setQuantity(getQuantity());
+            orderItem.setTax(taxAmount);
+            orderItem.setTaxedAmount(taxedAmount);
+            order.getItems().add(orderItem);
+
+            order.setTotal(order.getTotal().add(taxedAmount));
+            order.setTax(order.getTax().add(taxAmount));
+        }
     }
 }
--- src/main/java/it/gabrieletondi/telldontaskkata/useCase/SellItemsRequest.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/useCase/SellItemsRequest.java
@@ -1,11 +1,8 @@
 package it.gabrieletondi.telldontaskkata.useCase;
 
 import it.gabrieletondi.telldontaskkata.domain.Order;
-import it.gabrieletondi.telldontaskkata.domain.OrderItem;
-import it.gabrieletondi.telldontaskkata.domain.Product;
 import it.gabrieletondi.telldontaskkata.repository.ProductCatalog;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 public class SellItemsRequest {
@@ -21,26 +18,9 @@ public class SellItemsRequest {
 
     public void process(Order order, ProductCatalog productCatalog) {
         for (SellItemRequest itemRequest : getRequests()) {
-            Product product = productCatalog.getByName(itemRequest.getProductName());
-
-            if (product == null) {
-                throw new UnknownProductException();
-            }
-            else {
-                final BigDecimal taxedAmount = product.calculateTaxedAmount(itemRequest);
-                final BigDecimal taxAmount = product.calculateTaxAmount(itemRequest);
-
-                final OrderItem orderItem = new OrderItem();
-                orderItem.setProduct(product);
-                orderItem.setQuantity(itemRequest.getQuantity());
-                orderItem.setTax(taxAmount);
-                orderItem.setTaxedAmount(taxedAmount);
-                order.getItems().add(orderItem);
-
-                order.setTotal(order.getTotal().add(taxedAmount));
-                order.setTax(order.getTax().add(taxAmount));
-            }
+            itemRequest.process(order, productCatalog);
         }
 
     }
+
 }
